e51f8fc8484b9451ecd2abeeef149134badc4db9
DmitriyV <dmitrio87@mail.ru>
2024-03-27 06:42:54 +0300

    [master] добавление роутера

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/page" element={<Page />} />
+                </Routes>
+            </header>
+            <ul>
+                <li>
+                    <Link to="/">Главная </Link>
+                </li>
+                <li>
+                    <Link to="/page">Другая страница </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
+function Home() {
+    return <h1>Главная страница</h1>;
+}
+function Page() {
+    return <h1>Новая страница</h1>;
+}
 export default App;
